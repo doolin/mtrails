686f64d4280537cd9de58c0187859ec882c6a394
Dave Doolin <david.doolin@gmail.com>
2014-07-13 10:14:26 -0700

    started subdomain authentication

---
+++ subscribem/spec/support/subdomain_helpers.rb
@@ -0,0 +1,7 @@
+module SubdomainHelpers
+  def within_account_subdomain
+    let(:subdomain_url) { "http://#{account.subdomain}.example.com" }
+    before { Capybara.default_host = subdomain_url }
+    after { Capybara.default_host = "http://www.example.com" }
+  end
+end
