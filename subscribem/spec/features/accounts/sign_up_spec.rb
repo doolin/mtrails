41da8534ab672c81b719ceaea56bb4d33455e489
Dave Doolin <david.doolin@gmail.com>
2014-10-31 06:50:32 -0700

    Some sort of weird error on subdomain spec, use testem instead of test

--- subscribem/spec/features/accounts/sign_up_spec.rb
+++ subscribem/spec/features/accounts/sign_up_spec.rb
@@ -5,7 +5,7 @@ feature "Accounts" do
     visit subscribem.root_path
     click_link "Account Sign Up"
     fill_in "Name", :with => "Test"
-    fill_in "Subdomain", :with => "test"
+    fill_in "Subdomain", :with => "testem"
     fill_in "Email", with: "subscribem@example.com"
     fill_in "Password", with: 'password'
     fill_in "Password confirmation", :with => "password"
@@ -13,15 +13,15 @@ feature "Accounts" do
     success_message = "Your account has been successfully created."
     expect(page).to have_content(success_message)
     expect(page).to have_content("Signed in as subscribem@example.com")
-    expect(page.current_url).to eq("http://test.example.com/subscribem/")
+    expect(page.current_url).to eq("http://testem.example.com/subscribem/")
   end
 
   scenario "Ensure subdomain uniqueness" do
-    Subscribem::Account.create!(:subdomain => "test", :name => "Test")
+    Subscribem::Account.create!(:subdomain => "testem", :name => "Test")
     visit subscribem.root_path
     click_link "Account Sign Up"
     fill_in "Name", :with => "Test"
-    fill_in "Subdomain", :with => "test"
+    fill_in "Subdomain", :with => "testem"
     fill_in "Email", :with => "subscribem@example.com"
     fill_in "Password", :with => 'password'
     fill_in "Password confirmation", :with => 'password'
