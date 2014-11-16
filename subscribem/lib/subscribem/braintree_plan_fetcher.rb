8999cde8d20692a4698a000606edd37c10dde9b4
Dave Doolin <david.doolin@gmail.com>
2014-11-16 13:59:10 -0800

    Complete plan retrieval sections

--- subscribem/lib/subscribem/braintree_plan_fetcher.rb
+++ subscribem/lib/subscribem/braintree_plan_fetcher.rb
@@ -2,11 +2,18 @@ module Subscribem
   class BraintreePlanFetcher
     def self.store_locally
       Braintree::Plan.all.each do |plan|
-        Subscribem::Plan.create({
-          :name         => plan.name,
-          :price        => plan.price,
-          :braintree_id => plan.id
-        })
+        if local_plan = Subscribem::Plan.find_by(braintree_id: plan.id)
+          local_plan.update_attributes({
+            :name => plan.name,
+            :price => plan.price
+          })
+        else
+          Subscribem::Plan.create({
+            :name         => plan.name,
+            :price        => plan.price,
+            :braintree_id => plan.id
+          })
+        end
       end
     end
   end
--- subscribem/spec/integration/braintree_plan_helper_spec.rb
+++ subscribem/spec/integration/braintree_plan_helper_spec.rb
@@ -18,4 +18,17 @@ describe Subscribem::BraintreePlanFetcher do
     })
     Subscribem::BraintreePlanFetcher.store_locally
   end
+
+  it "checks and updates plans" do
+    expect(Braintree::Plan).to receive(:all).and_return([faux_plan])
+    expect(Subscribem::Plan).to receive(:find_by).
+      with(braintree_id: faux_plan.id).
+      and_return(plan = double)
+    expect(plan).to receive(:update_attributes).with({
+      :name => "Starter",
+      :price => "9.95"
+    })
+    expect(Subscribem::Plan).to_not receive(:create)
+    Subscribem::BraintreePlanFetcher.store_locally
+  end
 end
